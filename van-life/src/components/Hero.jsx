4d7e43fe844a4562b54a061684a3da738e026125
Milena Pieńkosz <designbymilencja@gmail.com>
2023-07-07 12:47:59 +0200

    feat(Hero): optimized hero

--- van-life/src/components/Hero.jsx
+++ van-life/src/components/Hero.jsx
@@ -5,14 +5,6 @@ import {dice, globe} from "../assets/index.js";
 import {headingLetters, spanTexts} from "../constants/index.js";
 
 const Hero = () => {
-    // const [showUnlockText, setShowUnlockText] = useState(true);
-    // useEffect(() => {
-    //     const timer = setTimeout(() => {
-    //         setShowUnlockText(false);
-    //     }, 4000);
-    //
-    //     return () => clearTimeout(timer);
-    // }, []);
     return (<>
             <section id="home" className={`flex md:flex-row flex-col ${styles.paddingY} h-[70vh] `}>
                 <div className={`flex-1 flex-col xl:px-0 sm:px-16 px-6`}>
@@ -29,21 +21,22 @@ const Hero = () => {
                         </video>
                         <div className="mt-[80px]">
                             <div className=" w-[100%] h-[100%] relative z-[5] text-white hidden md:block ">
-                                {headingLetters.map((letter, index)=> (
+                                {headingLetters.map((letter, index) => (
                                     <h2 key={index} className={`${styles.paragraph}`}>
                                         {letter}
                                         <span className="fade-span tracking-widest"> {spanTexts[index]} </span></h2>
                                 ))}
                             </div>
-                                <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient"></div>
-                                <div className="absolute z-[1] w-[10%] h-[10%] rounded-full bottom-40 white__gradient"></div>
-                                <div className="absolute z-[0] w-[50%] h-[50%] right-20 bottom-20  blue__gradient"></div>
+                            <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient"></div>
+                            <div
+                                className="absolute z-[1] w-[10%] h-[10%] rounded-full bottom-40 white__gradient"></div>
+                            <div className="absolute z-[0] w-[50%] h-[50%] right-20 bottom-20  blue__gradient"></div>
                         </div>
                     </div>
-                        <div className="flex flex-row justify-between items-center w-full text-center">
-                            <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100px] leading-[75px]">Welcome
-                                in <span className="text-gradient">OUR</span> world</h1>
-                        </div>
+                    <div className="flex flex-row justify-between items-center w-full text-center">
+                        <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100px] leading-[75px]">Welcome
+                            in <span className="text-gradient">OUR</span> world</h1>
+                    </div>
                 </div>
             </section>
         </>
