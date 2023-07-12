a5fd0d050f302f36a693acb392291380fad167b1
Milena Pieńkosz <designbymilencja@gmail.com>
2023-07-12 16:35:27 +0200

    feat(assets): changed image

--- van-life/src/assets/index.js
+++ van-life/src/assets/index.js
@@ -2,13 +2,19 @@ import plane from "./plane.svg";
 import earth from "./earth.svg";
 import planetOutline from "./planet-outline.svg";
 import dice from "./dice.svg";
-import globe from './globe.mp4';
 import logoFacebook from './logoFacebook.svg';
 import logoInstagram from './logoInstagram.svg';
-import logoEmail from './mail.svg'
-import logoLinkedin from './logoLinkedin.svg'
+import logoEmail from './mail.svg';
+import logoLinkedin from './logoLinkedin.svg';
+import hammer from './hammer.svg';
+import globe from './globe.mp4';
+import buildHome from './buildHome.mp4';
+import palmCamp from './palmCamp.jpg';
 
 export {
+    palmCamp,
+    buildHome,
+    hammer,
     globe,
     dice,
     planetOutline,
