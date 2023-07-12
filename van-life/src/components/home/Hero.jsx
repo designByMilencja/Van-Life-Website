e460568fb6822dae060834300ce3f1f108389c59
Milena Pieńkosz <designbymilencja@gmail.com>
2023-07-12 16:36:41 +0200

    fix(Hero): typo

--- van-life/src/components/home/Hero.jsx
+++ van-life/src/components/home/Hero.jsx
@@ -30,7 +30,7 @@ const Hero = () => {
                     </div>
                     <div className="flex flex-row justify-between items-center w-full text-center ">
                         <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100px] leading-[75px] ">Welcome
-                            in <span className="text-gradient">OUR</span> world</h1>
+                            in <span className="text-gradient">OUR</span> world...</h1>
                     </div>
                 </div>
             </section>
