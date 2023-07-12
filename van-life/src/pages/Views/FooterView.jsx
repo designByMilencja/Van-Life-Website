693f9545875d459021dfff4ebd4d2828726dabd0
Milena Pieńkosz <designbymilencja@gmail.com>
2023-07-12 11:20:04 +0200

    feat(footer): added footer

---
+++ van-life/src/pages/Views/FooterView.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+const FooterView = () => {
+    return (
+        <div className="flex flex-row p-6 rounded-[20px] footer">
+        <p className="w-full text-white text-center">Website made by me: <span className="text-gradient ml-2">DesignByMilencja</span> </p>
+        </div>
+    )
+}
+export default FooterView
