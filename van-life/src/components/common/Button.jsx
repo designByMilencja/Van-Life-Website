3bd84e7cb47791b71fa5f76efc6c0773864d5699
Milena Pieńkosz <designbymilencja@gmail.com>
2023-07-14 12:13:47 +0200

    feat(Button): added text props for button

--- van-life/src/components/common/Button.jsx
+++ van-life/src/components/common/Button.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-const Button = ({styles}) => {
+const Button = ({styles, text}) => {
     return (
-        <button type="button" className={`py-4 px-6 bg-blue-gradient font-poppins font-md text-[18px] text-primary outline-none ${styles}`}>Check</button>
+        <button type="button" className={`py-4 px-6 bg-blue-gradient font-poppins font-md text-[18px] text-primary outline-none ${styles}`}>{text}</button>
     )
 }
 export default Button
