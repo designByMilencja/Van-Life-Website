db87b7107414a53432b646b22ec2b6ba398faf0d
Milena Pieńkosz <designbymilencja@gmail.com>
2023-07-14 12:16:25 +0200

    feat(Header): changed margin

--- van-life/src/components/common/Header.jsx
+++ van-life/src/components/common/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
         <section className={`flex md:flex-row flex-col ${styles.paddingY} h-[65vh] w-full `}>
             <div className={`flex-1 flex-col xl:px-0 sm:px-16 px-6`}>
                 <Subheading icon={hammer} alt="icon hummer" firstText="Not" importantText="EXACTLY" secondText="home!"/>
-                <div className="flex">
+                <div className="flex p-2">
                         <img src={construction} alt="palms around camper" className="h-full items-end" />
                     <div className="mt-[80px]">
                         <div className=" w-[100%] h-[100%] z-[5] hidden md:block">
@@ -20,7 +20,7 @@ const Header = () => {
                         <div className="absolute z-[0] w-[20%] h-[70%] right-20 bottom-20  blue__gradient"></div>
                     </div>
                 </div>
-                <div className="flex flex-row justify-between items-center w-full text-center ">
+                <div className="flex flex-row justify-between items-center w-full text-center mt-5 ">
                     <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100px] leading-[75px] ">How
                         it<span className="text-gradient"> ALL </span> started...</h1>
                 </div>
