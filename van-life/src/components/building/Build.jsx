7d13fd7aadff09ff4ca44e80a5f480378a570386
Milena Pieńkosz <designbymilencja@gmail.com>
2023-07-17 23:12:04 +0200

    fix(components): changed path

--- van-life/src/components/building/Build.jsx
+++ van-life/src/components/building/Build.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import {Carousel} from "react-responsive-carousel";
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
-import styles from "../style.js";
+import styles from "../../style.js";
 
 const Build = () => {
     return (<>
