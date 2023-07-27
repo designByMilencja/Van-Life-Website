2aeea54921febd1e5afda19676f6911bd7317be8
Milena Pieńkosz <designbymilencja@gmail.com>
2023-07-27 18:34:49 +0200

    fest(ContactCard): added tabindex to use tab key

--- van-life/src/components/common/ContactCard.jsx
+++ van-life/src/components/common/ContactCard.jsx
@@ -5,7 +5,7 @@ const ContactCard = ({icon, title, href}) => {
         <div>
             <div className="bg-cyan-500 w-[90px] p-2 m-4 sm:mr-10 cursor-pointer rounded-[10px]">
                 <a href={href}>
-                    <img src={icon} alt="icon contact"/>
+                    <img tabIndex={1} src={icon} alt="icon contact"/>
                     <p className="text-base text-center">{title}</p>
                 </a>
             </div>
