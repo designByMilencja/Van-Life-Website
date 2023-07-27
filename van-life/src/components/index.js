7b8b3243fdb370ca6566da007442f320ce0a0f6e
Milena Pieńkosz <designbymilencja@gmail.com>
2023-07-27 18:37:36 +0200

    fix(): updating imports

--- van-life/src/components/index.js
+++ van-life/src/components/index.js
@@ -1,9 +1,7 @@
 import Hero from "./home/Hero.jsx";
 import Stats from "./home/Stats.jsx";
 import EntryTravel from "./home/EntryTravel.jsx";
-import CardDeal from "./CardDeal.jsx";
 import TravelCard from "./travelling/TravelCard.jsx";
-import Clients from "./Clients.jsx";
 import AboutUs from "./home/AboutUs.jsx";
 import Build from "./building/Build.jsx";
 import Gallery from "./gallery/Gallery.jsx";
@@ -13,9 +11,7 @@ export {
     AboutUs,
     Build,
     Gallery,
-    Clients,
     TravelCard,
     Stats,
     EntryTravel,
-    CardDeal
 }
