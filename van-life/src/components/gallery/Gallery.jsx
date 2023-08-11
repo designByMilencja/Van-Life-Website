028ac1f65e9ab03803b699c013ebbfee1cf5f104
Milena Pieńkosz <designbymilencja@gmail.com>
2023-08-11 12:02:55 +0200

    feat(Gallery): added handling moving from travel section to gallery section

--- van-life/src/components/gallery/Gallery.jsx
+++ van-life/src/components/gallery/Gallery.jsx
@@ -1,14 +1,20 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import Images from "./Images.jsx";
-import { imagesData } from "../../constants/index.js";
+import {imagesData} from "../../constants/index.js";
 import YearTabs from "./YearTabs.jsx";
 import CountryTabs from "./CountryTabs.jsx";
+import {useLocation} from "react-router-dom";
 
 const Gallery = () => {
     const years = Array.from(new Set(imagesData.map((data) => data.year)));
     const countries = Array.from(new Set(imagesData.map((data) => data.country)));
     const [selectedYear, setSelectedYear] = useState(years[0]);
     const [selectedCountry, setSelectedCountry] = useState(null);
+    const location = useLocation();
+    const queryParams = new URLSearchParams(location.search);
+    const queryName = queryParams.get("name");
+    const queryYear = queryParams.get("year");
+    console.log(queryYear, queryName)
 
     const handleYearSelect = (year) => {
         setSelectedYear(year);
@@ -18,17 +24,23 @@ const Gallery = () => {
     const handleCountrySelect = (country) => {
         setSelectedCountry(country);
     };
-
+    useEffect(() => {
+        handleYearSelect(queryYear);
+        handleCountrySelect(queryName);
+    }, []);
+    console.log(selectedYear, selectedCountry)
 
     return (
         <section id="gallery">
-            <YearTabs years={years} selectedYear={selectedYear} onYearSelect={handleYearSelect} />
-            {selectedYear && <CountryTabs countries={countries} selectedCountry={selectedCountry} onCountrySelect={handleCountrySelect} />}
+            <YearTabs id={`${setSelectedCountry}${setSelectedYear}`} years={years} selectedYear={selectedYear}
+                      onYearSelect={handleYearSelect}/>
+            {selectedYear && <CountryTabs countries={countries} selectedCountry={selectedCountry}
+                                          onCountrySelect={handleCountrySelect}/>}
             {imagesData
                 .filter((data) => data.year === selectedYear && (!selectedCountry || data.country === selectedCountry))
                 .map((data, index) => (
-                    <article key={index}>
-                        <Images images={data.images} />
+                    <article key={index} id={`${data.name}${data.year}`}>
+                        <Images images={data.images}/>
                     </article>
                 ))}
         </section>
