7ed9cca24abad67e558672465841f63d9e8e1324
Milena Pieńkosz <designbymilencja@gmail.com>
2023-08-12 23:03:44 +0200

    feat(ContactCard): changed margin and added target props

--- van-life/src/components/common/ContactCard.jsx
+++ van-life/src/components/common/ContactCard.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-const ContactCard = ({icon, title, href}) => {
+const ContactCard = ({icon, title, href, target}) => {
     return (
         <div>
-            <div className="bg-cyan-500 w-[90px] p-2 m-4 sm:mr-10 cursor-pointer rounded-[10px]">
-                <a href={href}>
+            <div className="bg-cyan-500 w-[80px] p-1 m-1 ss:m-3 sm:mr-10 cursor-pointer rounded-[10px]">
+                <a href={href} target={target}>
                     <img tabIndex={1} src={icon} alt="icon contact"/>
                     <p className="text-base text-center">{title}</p>
                 </a>
