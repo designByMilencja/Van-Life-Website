8d4b042d887ada8197c1045db111766f93c2acd9
Milena Pieńkosz <designbymilencja@gmail.com>
2023-08-12 23:07:20 +0200

    feat(Gallery): added scroll to back to the front of section

--- van-life/src/components/gallery/Gallery.jsx
+++ van-life/src/components/gallery/Gallery.jsx
@@ -4,43 +4,57 @@ import {imagesData} from "../../constants/index.js";
 import YearTabs from "./YearTabs.jsx";
 import CountryTabs from "./CountryTabs.jsx";
 import {useLocation} from "react-router-dom";
+import { animateScroll as scroll } from "react-scroll";
+
 
 const Gallery = () => {
     const years = Array.from(new Set(imagesData.map((data) => data.year)));
     const countries = Array.from(new Set(imagesData.map((data) => data.country)));
-    const [selectedYear, setSelectedYear] = useState(years[0]);
+    const [selectedYear, setSelectedYear] = useState(null);
     const [selectedCountry, setSelectedCountry] = useState(null);
     const location = useLocation();
     const queryParams = new URLSearchParams(location.search);
     const queryName = queryParams.get("name");
     const queryYear = queryParams.get("year");
-    console.log(queryYear, queryName)
 
+    const scrollToGallery = () => {
+        const scrollTimeout = setTimeout(() => {
+            scroll.scrollTo("gallery", {
+                smooth: 'easeInOutQuart',
+                duration: 900,
+                offset: -100,
+            });
+        }, 3000);
+
+        return () => clearTimeout(scrollTimeout);
+    };
+
+    useEffect(() => {
+        const parsedQueryYear = parseInt(queryYear, 10);
+        if (parsedQueryYear === 2022 || parsedQueryYear === 2023) {
+            setSelectedYear(parsedQueryYear);
+            setSelectedCountry(queryName);
+            scrollToGallery()
+        }
+    }, [queryYear, queryName]);
     const handleYearSelect = (year) => {
         setSelectedYear(year);
-        setSelectedCountry(null);
     };
 
     const handleCountrySelect = (country) => {
         setSelectedCountry(country);
     };
-    useEffect(() => {
-        handleYearSelect(queryYear);
-        handleCountrySelect(queryName);
-    }, []);
-    console.log(selectedYear, selectedCountry)
 
-    return (
-        <section id="gallery">
-            <YearTabs id={`${setSelectedCountry}${setSelectedYear}`} years={years} selectedYear={selectedYear}
+    return (<section>
+            <YearTabs years={years} selectedYear={selectedYear} selectedCountry={selectedCountry}
                       onYearSelect={handleYearSelect}/>
             {selectedYear && <CountryTabs countries={countries} selectedCountry={selectedCountry}
                                           onCountrySelect={handleCountrySelect}/>}
             {imagesData
                 .filter((data) => data.year === selectedYear && (!selectedCountry || data.country === selectedCountry))
                 .map((data, index) => (
-                    <article key={index} id={`${data.name}${data.year}`}>
-                        <Images images={data.images}/>
+                    <article id="gallery" key={index}>
+                        <Images selectedC={selectedCountry} selectedY={selectedYear} images={data.images}/>
                     </article>
                 ))}
         </section>
