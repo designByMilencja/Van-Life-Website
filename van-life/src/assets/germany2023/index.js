2b260320c0cd4dc9b71c792df81a7ff7c88948ce
Milena Pieńkosz <designbymilencja@gmail.com>
2023-08-14 16:06:26 +0200

    feat(): deleted no needed files

--- van-life/src/assets/germany2023/index.js
+++
@@ -1,7 +0,0 @@
-import e1 from './IMG_1814.png';
-import e2 from './IMG_1817.png';
-import e3 from './IMG_1824.png';
-import e4 from './IMG_1826.png';
-import e5 from './IMG_1840.png';
-
-export {e1,e2,e3,e4,e5}
