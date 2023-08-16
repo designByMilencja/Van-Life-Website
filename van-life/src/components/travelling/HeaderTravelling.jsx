4ef2d8eac929d388e45b10764d567cd9b73765d2
Milena Pieńkosz <designbymilencja@gmail.com>
2023-08-16 18:24:34 +0200

    feat(HeaderTravelling): changed alt name

--- van-life/src/components/travelling/HeaderTravelling.jsx
+++ van-life/src/components/travelling/HeaderTravelling.jsx
@@ -10,7 +10,7 @@ const HeaderTravelling = () => {
             <div className={`flex-1 flex-col xl:px-0 sm:px-16 px-6`}>
                 <Subheading icon={bulb} alt="icon hummer" firstText="Strange! We don't even" importantText="HAVE" secondText="an address!"/>
                 <div className="flex flex-col md:flex-row p-2">
-                    <img src="/travel/compass-5261062_640-min.jpg" alt="palm around camper van" className="h-full items-end md:mr-8 rounded-[10px]"/>
+                    <img src="/travel/compass-5261062_640-min.jpg" alt="image of compass on black background" className="h-full items-end md:mr-8 rounded-[10px]"/>
                     <div className="mt-[20px]">
                         <div className="absolute z-[0] w-[40%] h-[15%] top-40 pink__gradient"></div>
                         <div className="absolute z-[1] w-[10%] h-[20%] rounded-full bottom-40 white__gradient"></div>
